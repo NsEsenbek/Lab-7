d6175b88ca75fa428770fcdf2baceacb22b383b1
Nurtas <100025618+NsEsenbek@users.noreply.github.com>
2022-05-09 12:25:39 +0300

    Update music_player.py

--- music_player.py
+++ music_player.py
@@ -1,18 +1,13 @@
 from pygame import mixer
   
-# Starting the mixer
 mixer.init()
   
-# Loading the song
-mixer.music.load("crash.wav")
+mixer.music.load("song_name")
   
-# Setting the volume
-mixer.music.set_volume(0.7)
+mixer.music.set_volume(1)
   
-# Start playing the song
 mixer.music.play()
   
-# infinite loop
 while True:
       
     print("Press 'p' to pause, 'r' to resume")
